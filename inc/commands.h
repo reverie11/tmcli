0fcfea06a2155eb6d1ad005a5d00696c623fe5b5
Frederick Fortune Ong <frederickfortuneong@gmail.com>
2026-04-02 20:09:38 +0200

    feat: implement bash completion

---
+++ inc/commands.h
@@ -0,0 +1,36 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+typedef enum {
+    ADD, 
+    DEL,
+    MOD,
+    MOV,
+    SHW,
+    EXP,
+    RST,
+    N_CMDS,
+} Cmd_list;
+
+static const char *CMD_STR[N_CMDS] = {
+    [ADD] = "add", 
+    [DEL] = "delete",
+    [MOD] = "modify",
+    [MOV] = "move",
+    [SHW] = "show", 
+    [EXP] = "export", 
+    [RST] = "reset",
+};
+
+typedef enum{
+    START, 
+    END,
+    N_OBJS,
+} Obj_list;
+
+static const char *OBJ_STR[N_CMDS] = {
+    [START] = "start",
+    [END]   = "end",
+};
+
+#endif
--- src/main.c
+++ src/main.c
@@ -4,43 +4,77 @@
 #include <unistd.h>
 
 #include "colors.h"
+#include "commands.h"
 #include "tmcli.h"
 
 void print_help() {
     printf(
         "Usage: tmcli [OPTIONS] COMMAND [ARGS...]\n\n"
         "COMMANDS\n"
-        "  add    START END NAME     Add a new task with starttime START, \n"
-        "                               endtime END, and name NAME\n"
-        "  modify ID OBJ VAL         Modify the start- or endtime of an existing task\n"
-        "  move   ID VAL             Move existing task to different time\n"
-        "  del    ID                 Delete an existing task\n"
-        "  show                      Show all tasks\n"
-        "  export                    export to ICS-Format (.ics)\n"
-        "  reset                     Reset TMCLI's state\n"
+        "  add      START END NAME     Add a new task with starttime START, \n"
+        "                                 endtime END, and name NAME\n"
+        "  modify   ID    OBJ VAL      Modify the start- or endtime of an existing task\n"
+        "  move     ID    VAL          Move existing task to different time\n"
+        "  delete   ID                 Delete an existing task\n"
+        "  show                        Show all tasks\n"
+        "  export                      export to ICS-Format (.ics)\n"
+        "  reset                       Reset TMCLI's state\n"
         "\n"
         "OBJECTS\n"
-        " start                      task's starttime\n"
-        " end                        task's endtime\n"
+        " start                        task's starttime\n"
+        " end                          task's endtime\n"
         "\n"
         "OPTIONS\n"
-        "  -v, --verbose             Enable verbose output\n"
-        "  -h, --help                Show this help message\n"
+        "  -v, --verbose               Enable verbose output\n"
+        "  -h, --help                  Show this help message\n"
         "\n"
         "Version  :  1.0.0\n"
         "Author   :  reverie\n"
     );
 }
 
+void print_completion(int argc, char** argv) {
+
+    TM_state tms;
+
+    // quick fetch state
+    FILE *fp = fopen(STATE_FILE, "r");
+    if(!fp) return;
+    size_t bytes = fread(&tms, 1, sizeof(TM_state), fp);
+    fclose(fp);
+    if(bytes < sizeof(TM_state)) return;
+
+    if(argc == 0){
+       for(int i = 0; i < N_CMDS; i++) printf("%s ", CMD_STR[i]);
+    } else if(argc == 1){
+        if(strcmp(argv[0], CMD_STR[MOD]) == 0 ||
+           strcmp(argv[0], CMD_STR[MOV]) == 0 ||
+           strcmp(argv[0], CMD_STR[DEL]) == 0 ){
+            for(int i = 0; i < tms.tm.n_active_tasks; i++) printf("%d ", i);
+        }
+    } else if(argc == 2){
+        if( strcmp(argv[0], CMD_STR[MOD]) == 0 ){
+            for(int i = 0; i < N_OBJS; i++) printf("%s ", OBJ_STR[i]);
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
-    char msg[MSG_MAXLEN];
-    bzero(msg, sizeof(msg));
     g_verbose = 0;
 
+    char msg[MSG_MAXLEN];
+    bzero(msg, sizeof(msg));
+
+    // strictly meant for bash completion
+    if(argc > 1 && (strcmp(argv[1], "--complete-args") == 0)){
+        print_completion(argc-2, argv+2);
+        goto cleanup;
+    }
+
     static struct option long_options[] = {
-        {"verbose", no_argument,       0, 'v'},
-        {"help",    no_argument,       0, 'h'},
+        {"verbose",         no_argument,       0,   'v'},
+        {"help",            no_argument,       0,   'h'},
         {0, 0, 0, 0}
     };
 
@@ -70,7 +104,7 @@ int main(int argc, char** argv)
         goto error_handling;
     }
 
-    if(strcmp(cmd, "add") == 0){
+    if(strcmp(cmd, CMD_STR[ADD]) == 0){
         if(n_args < 3) goto error_handling;
         char* start = argv[optind++];
         char* end = argv[optind++];
@@ -81,14 +115,14 @@ int main(int argc, char** argv)
         TM_print_all_tasks_highlight(&tm, 0, id);
         TM_save_state(&tm);
 
-    } else if(strcmp(cmd, "del") == 0){
+    } else if(strcmp(cmd, CMD_STR[DEL]) == 0){
         if(n_args < 1) goto error_handling;
         int order_id = str_to_uint(argv[optind++]);
         if(order_id == -1) goto error_handling;
         TM_delete_task(&tm, order_id);
         TM_save_state(&tm);
 
-    } else if(strcmp(cmd, "modify") == 0){
+    } else if(strcmp(cmd, CMD_STR[MOD]) == 0){
         if(n_args < 3) goto error_handling;
 
         int order_id = str_to_uint(argv[optind++]);
@@ -113,7 +147,7 @@ int main(int argc, char** argv)
         TM_print_all_tasks_highlight(&tm, 0, id);
         TM_save_state(&tm);
 
-    } else if(strcmp(cmd, "move") == 0){
+    } else if(strcmp(cmd, CMD_STR[MOV]) == 0){
         if(n_args < 2) {
             goto error_handling;
         }
@@ -133,7 +167,7 @@ int main(int argc, char** argv)
         TM_print_all_tasks_highlight(&tm, 0, id);
         TM_save_state(&tm);
 
-    } else if(strcmp(cmd, "show") == 0){
+    } else if(strcmp(cmd, CMD_STR[SHW]) == 0){
         if(g_verbose) TM_print_self(&tm);
         if(n_args > 0){
             int mode = str_to_uint(argv[optind++]);
@@ -143,7 +177,7 @@ int main(int argc, char** argv)
             TM_print_all_tasks_highlight(&tm, 0, curr_taskid);
         }
 
-    } else if(strcmp(cmd, "export") == 0){
+    } else if(strcmp(cmd, CMD_STR[EXP]) == 0){
         int status = TM_export_to_ICS(&tm);
         if(status == -1){
             snprintf(msg, sizeof(msg), "export failed");
@@ -151,21 +185,21 @@ int main(int argc, char** argv)
         fprintf(stdout, CYAN "INFO: " RESET "%s\n", msg);
 
     } else if(strcmp(cmd, "reset") == 0){
-        TM_delete_all_tasks(&tm);
         unlink(STATE_FILE);
         snprintf(msg, sizeof(msg), "task list reseted");
         fprintf(stdout, CYAN "INFO: " RESET "%s\n", msg);
-        return 0;
 
     } else {
         goto error_handling;
     }
+
+cleanup:
     TM_delete_all_tasks(&tm);
-    
     return 0;
+
 error_handling:
-    TM_delete_all_tasks(&tm);
     print_help();
+    TM_delete_all_tasks(&tm);
     return 1;
 }
 
