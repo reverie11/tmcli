27d4fc93d0382a003aeeddf941f3d67a5ab05c92
Frederick Fortune Ong <frederickfortuneong@gmail.com>
2026-04-02 20:11:11 +0200

    feat: installation script include bash completion

--- install.sh
+++ install.sh
@@ -2,18 +2,20 @@
 
 set -e 
 
-if [ -z "$XDG_CACHE_HOME" ]; then
-    export XDG_CACHE_HOME="$HOME/.cache"
-fi
+export BIN_DIR="$HOME/.local/bin/"
+export STATE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/tmcli/"
+export COMPLETION_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions/"
 
-mkdir -p $XDG_CACHE_HOME/tmcli
+mkdir -p $STATE_DIR
+mkdir -p $COMPLETION_DIR
 
 make clean
 if command -v bear &>/dev/null; then
-    bear -- make STATE_FILE=$XDG_CACHE_HOME/tmcli/state.dat
+    bear -- make STATE_FILE=$STATE_DIR/state.dat
 else
-    make STATE_FILE=$XDG_CACHE_HOME/tmcli/state.dat
+    make STATE_FILE=$STATE_DIR/state.dat
 fi
 
-cp ./build/bin/tmcli $HOME/.local/bin/
+cp ./build/bin/tmcli $BIN_DIR/tmcli
+cp ./tmcli-completion $COMPLETION_DIR/tmcli
 hash -r
