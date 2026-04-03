f0ed4842cb8f134d5dee84988ca27743b8db9978
Frederick Fortune Ong <frederickfortuneong@gmail.com>
2026-04-03 14:32:10 +0200

    uninstallation also removes completion file

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -e 
-rm -rf $XDG_CACHE_HOME/tmcli
-rm -f $HOME/.local/bin/tmcli
+
+export BIN_FILE="$HOME/.local/bin/tmcli"
+export STATE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/tmcli/"
+export COMPLETION_FILE="${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions/tmcli"
+
+rm -rf $STATE_DIR
+rm -f $BIN_FILE
+rm -f $COMPLETION_FILE
 hash -r
