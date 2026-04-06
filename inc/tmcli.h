2eaade99945e78c0638c89c863a5eaf7300e5c73
reverie11 <frederickfortuneong@gmail.com>
2026-04-06 12:32:47 +0200

    docs: reorganize code documentation

--- inc/tmcli.h
+++ inc/tmcli.h
@@ -4,12 +4,13 @@
 #include "types.h"
 
 /**
- * @brief TM_init
+ * @brief TM_init.
  */
 int TM_init(TaskManager* tm);
 
+/******************************** Task ***************************************/
 /**
- * @brief TM_create_task
+ * @brief TM_create_task.
  * create task dynamically with malloc()
  * @return task_id on success, otherwise -1
  * @todo Add max_task_check
@@ -18,18 +19,18 @@ int TM_create_task(TaskManager* tm, const Time start, const Time end, const
         char* name);
 
 /**
- * @brief TM_delete_task
+ * @brief TM_delete_task.
  * delete task with free()
  */
 int TM_delete_task(TaskManager* tm, int task_order_id);
 
 /**
- * @brief
+ * @brief TM_delete_all_tasks.
  */
 int TM_delete_all_tasks(TaskManager* tm);
 
 /**
- * @brief
+ * @brief TM_modify_task_start.
  * modify task_start WITHOUT sorting the task_list
  */
 int TM_modify_task_start(TaskManager* tm, int task_order_id, Time start);
@@ -37,6 +38,19 @@ int TM_modify_task_end(TaskManager* tm, int task_order_id, Time end);
 int TM_modify_task_name(TaskManager* tm, int task_order_id, const char* name);
 int TM_move_task_start(TaskManager* tm, int task_order_id, Time start);
 
+/**
+ * @brief TM_sort_tasks.
+ */
+int TM_sort_tasks(TaskManager* tm);
+
+/******************************** GET ****************************************/
+/**
+ * @brief TM_get_curr_taskid
+ * get the order_id of current ongoing task
+ */
+int TM_get_curr_taskid(TaskManager* tm);
+
+/******************************** STATE **************************************/
 /**
  * @brief
  */
@@ -50,10 +64,11 @@ int TM_restore_state(TaskManager* tm);
 /**
  * @brief
  */
-int TM_sort_tasks(TaskManager* tm);
+int TM_reset_state(TaskManager* tm);
 
+/******************************** PRINT **************************************/
 /**
- * @brief
+ * @brief TM_print_task.
  */
 void TM_print_task(TaskManager* tm, int task_order_id);
 
@@ -67,19 +82,14 @@ void TM_print_all_tasks_highlight(TaskManager* tm, int mode, int highlight_id);
 #define TM_print_all_tasks(tm, mode) TM_print_all_tasks_highlight(tm, mode, -1)
 
 /**
- * @brief
+ * @brief TM_print_self.
  */
 void TM_print_self(TaskManager* tm);
 
+/******************************** MISCELLANEOUS ******************************/
 /**
- * @brief
+ * @brief TM_export_to_ICS.
  */
 int TM_export_to_ICS(TaskManager* tm);
-
-/**
- * @brief TM_get_curr_taskid
- * get the order_id of current ongoing task
- */
-int TM_get_curr_taskid(TaskManager* tm);
 
 #endif
--- inc/utils.h
+++ inc/utils.h
@@ -5,7 +5,7 @@
 
 /******************************** VALIDATION *********************************/
 /**
- * @brief validate_task_time
+ * @brief validate_task_time.
  * @return 0 if valid, 1 otherwise
  */
 int validate_task_time(const Task* task);
@@ -17,7 +17,7 @@ int validate_task_time(const Task* task);
 int validate_date(const Date date);
 
 /**
- * @brief validate_str_format
+ * @brief validate_time_format.
  * this function just validate the string, is NOT responsible for the
  * semantic validation of the time.
  *
@@ -27,36 +27,68 @@ int validate_date(const Date date);
  *
  * @return 0 if valid, 1 otherwise
  */
-int validate_str_format(const char* str);
+int validate_time_format(const char* str);
+
+/**
+ * @brief validate_time_format.
+ * this function just validate the string, is NOT responsible for the
+ * semantic validation of the time.
+ *
+ * @notes
+ * OK: "20", "99.99", "99.99.9999", "2"
+ * NOT_OK: "100", "2.", "20.", "20.1", "20.100.1000", "20.20.99999"
+ *
+ * @return 0 if valid, 1 otherwise
+ */
+int validate_date_format(const char* str);
 
 /******************************** CALCULATION ********************************/
 /**
- * @brief calculate_task_duration
+ * @brief calculate_task_duration.
  */
 float calculate_task_duration(const Task* task);
 
+/**
+ * @brief calculate_task_duration.
+ */
 Time calculate_end_time(const Time start, float duration_h);
 
 /******************************** COMPARISON *********************************/
 /**
- * @brief compare_and_reorder_tasks
+ * @brief compare_and_reorder_tasks.
  * helper function for qsort()
  * @notes
- * =0 if priority(a) = priority(b)
- * <0 if priority(a) > priority(b)
- * >0 if priority(a) < priority(b)
+ * =0 if priority(a) = priority(b).
+ * <0 if priority(a) > priority(b) | a is earlier.
+ * >0 if priority(a) < priority(b) | a is later.
  */
 int compare_and_reorder_tasks(const void* a, const void* b);
 int compare_time(const void* a, const void* b);
-
+int compare_date(const void* a, const void* b);
 /******************************** CONVERSION *********************************/
 /**
- * @brief str_to_time
+ * @brief str_to_time.
  */
 Time str_to_time(const char* str);
 
 /**
- * @brief str_to_uint
+ * @brief str_to_time.
+ * This function checks the semantic validity of the str with valid date format
+ * and coverts it to date if valid. In the case of shorthand format, this
+ * function will always assume future date, not one in the past. Only explicit
+ * format can be converted into past date.
+ * @notes examples:
+ * today is 18.12.2026.
+ * "17"         -> 17.01.2027.
+ * "17.12"      -> 17.12.2027. 
+ * "19.11"      -> 19.11.2027.
+ * "18.12"      =  18.12.2026.
+ * "18.12.2026" =  18.12.2026.
+ */
+Date str_to_date(const char* str);
+
+/**
+ * @brief str_to_uint.
  * @notes
  * str can be max 32 char long.
  * only accepts all_digit_string
@@ -66,18 +98,25 @@ long str_to_uint(const char* str);
 
 /******************************** BOOLEAN ************************************/
 /**
- * @brief
+ * @brief str_is_digit.
  */
 int str_is_digit(const char* str);
 
 /**
- * @brief
+ * @brief ch_is_digit.
  */
 int ch_is_digit(int ch);
 
+/**
+ * @brief is_leap_year.
+ */
 bool is_leap_year(const int year);
 
 /******************************** GET ****************************************/
+
+/**
+ * @brief get_date_today.
+ */
 Date get_date_today(void);
 
 #endif
